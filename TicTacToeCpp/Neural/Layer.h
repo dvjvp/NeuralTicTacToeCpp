cff3a400d82df88ae9c993c456b7078eb549a055
Daniel Janowski <daniel.janowski@666510.com>
2017-09-16 13:42:44 +0200

    Documented Neural/Layer.h and Neural/NetworkHelpers.h

--- TicTacToeCpp/Neural/Layer.h
+++ TicTacToeCpp/Neural/Layer.h
@@ -6,23 +6,76 @@
 
 namespace Neural
 {
+	/*
+	* A single neural network layer.
+	* It should probably be just a container for its neurons, but i figured
+	* it'd be just easier if I'd be just storing theoretical neurons' values
+	* directly in a layer in buffers.
+	*/
 	template<typename T>
 	class Layer
 	{
 	public:
+		/**
+		* @brief Default constructor
+		*
+		* Does nothing. Actual initialisation is performed by Initialize() method and
+		* it should be called right after constructing a layer. That way we can create
+		* array of layers using @c new operator!
+		*/
 		Layer();
+		/**
+		* @brief Actual array initialization. Basically a second c-tor.
+		* 
+		* Allocates buffers for neuron values. Doesn't clear them because why
+		* waste time. You can clear it manually, fill it with random
+		* values or copy values to it yourself if you need that. No need to
+		* do the same thing twice.
+		* @param thisLayerSize - how many neurons should be in this layer
+		* @param nextLayerSize - how many neurons should be in next layer. 
+		* Should be 0 for output layer
+		*/
 		void Initialize(const size_t thisLayerSize, const size_t nextLayerSize);
+		/**
+		* @brief Default destructor
+		* 
+		* frees buffers allocated in Initialize() function.
+		*/
 		~Layer();
 
+		
+		/**
+		* @brief Clears input buffer
+		* 
+		* Fills input buffer with 0-s. Uses memset, so it should be pretty quick
+		*/
 		void ClearInputs();
 
+		/**
+		* @brief Gets weight from @p nodeInThisLayerIndex neuron here to @p nodeInNextLayerIndex neuron in next layer
+		* 
+		* It's just a wrapper so there's no need to unwrap value from 2d array (stored as singular array) by hand every time 
+		* 
+		* @param nodeInThisLayerIndex - index of node in this layer from which output should the weight be taken
+		* @param nodeInNextLayerIndex - index of node in next layer (target of connection we want to know weight of)
+		* @return weight of connection between neurons
+		*/
 		inline T GetWeight(const size_t nodeInThisLayerIndex, const size_t nodeInNextLayerIndex) const;
 	public:
+		//Buffer for tresholds (minimal values needed to pass signal forward by neuron).
+		//There's one for each neuron, so there's (thisLayerSize) of them
 		T* tresholds;
+		//Buffer for weights (value by which to multiply the input before passing it forward) from all neurons
+		//in this layer to all neurons in next layer. There's (nextLayerSize) of them for each neuron, so total
+		// of (thisLayerSize*nextLayerSize).
 		T* weights;
+		//Buffer for temporary values used by network during computation.
+		//There's one for each layer, so there's (thisLayerSize) of them
 		T* inputs;
 
+		//Number of neurons in this layer
 		size_t thisLayerSize;
+		//Number of neurons in next layer. It's 0 if this is the output layer
 		size_t nextLayerSize;
 	};
 }
--- TicTacToeCpp/Neural/NetworkHelpers.h
+++ TicTacToeCpp/Neural/NetworkHelpers.h
@@ -5,12 +5,28 @@ namespace Neural
 {
 	namespace HelperFunctions
 	{
+		/**
+		* Gets random number between 0 and @p max
+		* Should probably be used with floating point types.
+		* It probably won't work with integers (i guess it'd return 0).
+		* @param max - maximum possible value to return [exclusive]
+		* @return Some random value between 0 and @p max
+		*/
 		template<typename T>
 		inline T Random(T max)
 		{
 			return ((T)rand() / (T)(RAND_MAX)) * max;
 		}
 
+
+		/**
+		* @brief Randomizes threshold values of each neuron in NeuralNetwork
+		* 
+		* Sets value of each neuron.threshold in NeuralNetwork to some random value between @p min and @p max
+		* @param network - network to randomize
+		* @param min - minimal value possible [inclusive]
+		* @param max - maximum value possible [exclusive]
+		*/
 		template<typename T>
 		void RandomizeNeuralNetworkTresholds(NeuralNetwork<T>* network, const T min, const T max)
 		{
@@ -28,6 +44,14 @@ namespace Neural
 			}
 		}
 
+		/**
+		* @brief Randomizes weights values of each neuron in NeuralNetwork
+		*
+		* Sets value of each neuron.weight in NeuralNetwork to some random value between @p min and @p max
+		* @param network - network to randomize
+		* @param min - minimal value possible [inclusive]
+		* @param max - maximum value possible [exclusive]
+		*/
 		template<typename T>
 		void RandomizeNeuralNetworkWeights(NeuralNetwork<T>* network, const T min, const T max)
 		{
